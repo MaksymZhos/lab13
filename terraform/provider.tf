fc7f2317bdbcc69824a6f63627cab85fda3047bd
IzunaJos <zhosmaxim@gmail.com>
2025-04-04 09:50:47 -0700

    Add files via upload

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "lab13-acit4640-bucket"
+    key     = "terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
